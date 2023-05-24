7a08e707fadcfdc15941869e00a7d6f7b26dd17b
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-24 22:13:41 +0200

    Added Fixture Test

---
+++ cypress/e2e/Tc12_FixturesTest.spec.cy.js
@@ -0,0 +1,25 @@
+describe('Fixtures test',function(){
+
+    beforeEach(function(){
+        cy.fixture('ConduitData').as('data')
+    })
+
+    it('Conduit - Valid Credentials',function(){
+        cy.visit('https://react-redux.realworld.io/')
+        cy.contains('Sign in').click()
+        cy.get('input[type="email"]').type(this.data.email)
+        cy.get('input[type="password"]').type(this.data.password)
+        cy.get('button[type="submit"]').click()
+        cy.contains('Settings').click()
+        cy.contains('Or click here to logout.').click()
+    })
+
+    it('Conduit - Invalid Credentials',function(){
+        cy.visit('https://react-redux.realworld.io/')
+        cy.contains('Sign in').click()
+        cy.get('input[type="email"]').type(this.data.email)
+        cy.get('input[type="password"]').type(this.data.passwordWrong)
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-messages').should('contain','email or password is invalid')
+    })
+})
