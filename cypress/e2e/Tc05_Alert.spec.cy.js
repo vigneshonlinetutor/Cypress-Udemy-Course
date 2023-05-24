a133c93d3da198056b34b95f6601a0a563a7eb10
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-24 21:46:14 +0200

    Added Alert Test

---
+++ cypress/e2e/Tc05_Alert.spec.cy.js
@@ -0,0 +1,40 @@
+describe('Advanced UI Elements',function(){
+
+    it('Simple alert',function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.contains('Click for JS Alert').click()
+        cy.on('window:alert',(str)=>{
+            expect(str).to.equal('I am a JS Alert')
+        })
+        cy.get('#result').should('contain','You successfully clicked an alert')
+        
+    })
+
+    it('Confirmation alert - Ok button',function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.contains('Click for JS Confirm').click()
+        cy.on('window:confirm',(str)=>{
+            expect(str).to.equal('I am a JS Confirm')
+        })
+        cy.get('#result').should('contain','You clicked: Ok')
+    })
+
+    it('Confirmation alert - Cancel button',function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.contains('Click for JS Confirm').click()
+        cy.on('window:confirm',(str)=>{
+            expect(str).to.equal('I am a JS Confirm')
+            return false
+        })
+        cy.get('#result').should('contain','You clicked: Cancel')
+    })
+
+    it('Prompt Alert',function(){
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.window().then(function($win){
+            cy.stub($win,'prompt').returns('Hello Alert')
+            cy.contains('Click for JS Prompt').click()        
+        })
+        cy.get('#result').should('contain','You entered: Hello Alert')
+    })
+})
