a238004a33ec0448c438ce80fe7b96f0e15c1f2e
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-24 22:06:46 +0200

    Added Browser control test

---
+++ cypress/e2e/Tc09_BrowserControl.spec.cy.js
@@ -0,0 +1,13 @@
+describe('Advanced UI Elements',function(){
+
+    it('Browser Controls',function(){
+        cy.visit('https://react-redux.realworld.io/')
+        cy.contains('Sign in').click()
+        cy.go('back')
+        cy.go('forward')
+        cy.go(-1)
+        cy.go(1)
+        cy.get('input[type="email"]').type('cypress')
+        cy.reload()
+    })
+})
