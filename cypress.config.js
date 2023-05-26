23884823c226af80c638ee2a1d4280cb14f8ba95
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-26 20:32:05 +0200

    Added Dashboard Feature

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  screenshotOnRunFailure:false,
-  video:false,
+  projectId: 'zoivxh',
   e2e: {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on,config)
