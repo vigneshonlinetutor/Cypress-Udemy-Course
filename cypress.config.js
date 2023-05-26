08e66b1fbacc1afce3cf834716bf1e3cd6fd2728
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-26 21:13:20 +0200

    Added mochawesome Report

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'zoivxh',
+  "reporter": "cypress-multi-reporters",
+  "reporterOptions":{
+    "reporterEnabled": "mochawesome",
+    "mochawesomeReporterOptions":{
+      "reportDir": "cypress/reports/mocha",
+      "quiet": true,
+      "overwrite": false,
+      "html": false,
+      "json": true
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on,config)
