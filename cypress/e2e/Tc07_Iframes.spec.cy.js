8f005ff5cf9721eafcd30e49648863a9cc66141e
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2024-02-06 13:32:30 +0100

    Updated Frames test

--- cypress/e2e/Tc07_Iframes.spec.cy.js
+++ cypress/e2e/Tc07_Iframes.spec.cy.js
@@ -5,8 +5,10 @@ describe('Advanced UI Elements',function(){
     it('Iframes',function(){
         cy.visit('https://the-internet.herokuapp.com/iframe')
         cy.get('#mce_0_ifr').within(function($iframe){
+            //Adding Wait for page to load fully
+            cy.wait(500)
             const frame = $iframe.contents().find('#tinymce')
-            cy.wrap(frame).clear().type('hello')
+            cy.wrap(frame).clear().type("Hello")
         })
     })
 })
